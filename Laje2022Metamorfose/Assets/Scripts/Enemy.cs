434c4e0ab0a45321b2942e58b2e650516418301f
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2022-08-10 13:16:42 -0300

    Movimento do Sapo corrigido

--- Laje2022Metamorfose/Assets/Scripts/Enemy.cs
+++ Laje2022Metamorfose/Assets/Scripts/Enemy.cs
@@ -2,27 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour{
+public class Enemy : MonoBehaviour
+{
     public GameObject player;
     public float moveSpeed = 2f;
     private Rigidbody2D rb;
     private Vector2 movement;
-
-    // Start is called before the first frame update
-    void Start(){
-        rb = this.GetComponent<Rigidbody2D>();
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update(){
+    
+    void Update()
+    {
         Vector3 direction = player.transform.position - transform.position;
         direction.Normalize();
         movement = direction;
     }
-    private void FixedUpdate() {
-        moveCharacter(movement);
+    
+    private void FixedUpdate()
+    {
+        MoveCharacter(movement);
     }
-    void moveCharacter(Vector2 direction) {
-        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed));
+    
+    void MoveCharacter(Vector2 direction)
+    {
+        rb.velocity = direction * moveSpeed;
     }
 }
